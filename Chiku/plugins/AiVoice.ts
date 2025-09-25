ec2cfcfb7971f93a84152109abfafdc4247149a8
itsdevil-dev <muralimurali3211260@gmail.com>
2025-09-25 19:11:41 +0530

    Update AiVoice.ts

--- Chiku/plugins/AiVoice.ts
+++ Chiku/plugins/AiVoice.ts
@@ -8,8 +8,7 @@ async function GetAiVoice(chatId: number, text: string): Promise<void> {
     const X = await ChikuAi.send_message(chatId, "Generating Artificial Intelligence Voice For Your Text🔊..");
      const Id = X.result?.message_id;
     const encodedText = encodeURIComponent(text);
-    const ttsApiUrl = `https://advanced-tts.darkhacker7301.workers.dev/?message=${encodedText}&voice=en-US-Wavenet-C&type=url`;
-
+    const ttsApiUrl = `https://advanced-tts.darkhacker7301.workers.dev/?action=create&text=${encodedText}&voice=Aditi&type=url`;
 
     const response = await fetch(ttsApiUrl);
     if (!response.ok) {
